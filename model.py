efb83ace513a9495cc6ed6a862989e7df2eae0b6
pradeep <pradeep.misra@gmail.com>
2024-09-13 15:06:38 +0530

    Updated the model

--- model.py
+++ model.py
@@ -1,28 +1,11 @@
-from sklearn2pmml import sklearn2pmml
-from sklearn2pmml.pipeline import PMMLPipeline
-from sklearn.linear_model import LogisticRegression
-import os
-import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-display.print_sqlmr_query = True
-passwd = "alice"
-uid = "alice"
+x = np.array([100,200,300,400,500]).reshape(-1,1)
+y = np.array([150,200,250,300,350])
 
-con = create_context(host="server@mydomain.com", username=uid, password=passwd)
-con
+model = LinearRegression()
+model.fit(x,y)
 
-train_df = DataFrame.from_query("select * from iris_train")
-traid_pd = train_df.to_pandas()
-traid_pd
-type(traid_pd)
-
-X = traid_pd[['sepal_length','sepal_width','petal_length', 'petal_width' ]]
-y=traid_pd[['species']]
-
-#pipeline
-pipeline = PMMLPipeline([
-     ("classifier", LogisticRegression(random_state=0))
-     ])
-
-pipeline.fit(X,y)
-sklearn2pmml(pipeline,"../sql/iris_db_glm_model.pmml",with_repr=True)
+y_prod = model.predict(x)
